728d865b76d7b366947d2ddc594b290c792dd744
elvisdickmann <72405119+elvisdickmann@users.noreply.github.com>
2024-02-23 00:06:03 -0300

    Enviando site Echos

--- Projeto Echo's Hotel/script.js
+++ Projeto Echo's Hotel/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-})
+    $(".owl-carousel").owlCarousel({
+        items:4,
+        loop:true,
+        nav: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                dots: true,
+                
+            },
+            600:{
+                items:4,
+                dots: false,
+            }
+        }
+    });
+});
